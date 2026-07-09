d3f1fa7c84c06b8506aec1b6a155b8d31f8888a8
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-07-09 19:48:58 +0600

    create new student by serializer

--- api/views.py
+++ api/views.py
@@ -7,9 +7,15 @@ from .serializers import StudentSerializer
 
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["GET","POST"])
 def view_students(request):
     if request.method == "GET":
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
